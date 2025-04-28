f20c02965946af69299e242933cf2d5ef76f7747
Jomaa Abdulrahman <abdulrahman.jomaa@ulb.be>
2025-04-28 16:26:00 +0200

    g ecrit la classe graph. c'est un contenaire de node (vertices) avec des operations.

---
+++ src/graphs/Graph.java
@@ -0,0 +1,24 @@
+package graphs;
+
+import java.util.ArrayList;
+
+public class Graph {
+    private ArrayList<Node> vertices;
+    
+
+    public Graph(){
+        this.vertices = new ArrayList<>();
+    }
+
+    public ArrayList<Node> getVertices(){
+        return vertices;
+    }
+
+    public boolean addVertex(Node vertex){
+        return vertices.add(vertex);
+    }
+
+    public boolean removeVertex(Node vertex){
+        return vertices.remove(vertex);
+    }
+}
