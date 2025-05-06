ec0caa3036ad804fc4a36df766cf0e826831eeb0
Jomaa Abdulrahman <abdulrahman.jomaa@ulb.be>
2025-05-06 19:30:22 +0200

    on affiche toute la belgique

--- src/MainApp.java
+++ src/MainApp.java
@@ -39,54 +39,36 @@ public class MainApp {
         String TECRoutesFilePath = "src/database/GTFS/TEC/routes.csv";
 
         List<Stop> Delijn_stops = CSVReader.loadStops(DELIJNStopsFilePath);
-        List<StopTime> Delijn_stopTimes = CSVReader.loadStopTimes(DELIJNStopTimesFilePath);
-        Map <String, String> Delijn_trips = CSVReader.loadTrips(DELIJNTripsFilePath);
-        Map <String, Route> Delijn_routes = CSVReader.loadRoutes(DELIJNRoutesFilePath);
+        // List<StopTime> Delijn_stopTimes = CSVReader.loadStopTimes(DELIJNStopTimesFilePath);
+        // Map <String, String> Delijn_trips = CSVReader.loadTrips(DELIJNTripsFilePath);
+        // Map <String, Route> Delijn_routes = CSVReader.loadRoutes(DELIJNRoutesFilePath);
 
         List<Stop> Sncb_stops = CSVReader.loadStops(SNCBStopsFilePath);
-        List<StopTime> Sncb_stopTimes = CSVReader.loadStopTimes(SNCBStopTimesFilePath);
-        Map <String, String> Sncb_trips = CSVReader.loadTrips(SNCBTripsFilePath);
-        Map <String, Route> Sncb_routes = CSVReader.loadRoutes(SNCBRoutesFilePath);
+        // List<StopTime> Sncb_stopTimes = CSVReader.loadStopTimes(SNCBStopTimesFilePath);
+        // Map <String, String> Sncb_trips = CSVReader.loadTrips(SNCBTripsFilePath);
+        // Map <String, Route> Sncb_routes = CSVReader.loadRoutes(SNCBRoutesFilePath);
 
         List<Stop> Stib_stops = CSVReader.loadStops(STIBStopsFilePath);
-        List<StopTime> Stib_stopTimes = CSVReader.loadStopTimes(STIBStopTimesFilePath);
-        Map <String, String> Stib_trips = CSVReader.loadTrips(STIBTripsFilePath);
-        Map <String, Route> Stib_routes = CSVReader.loadRoutes(STIBRoutesFilePath);
+        // List<StopTime> Stib_stopTimes = CSVReader.loadStopTimes(STIBStopTimesFilePath);
+        // Map <String, String> Stib_trips = CSVReader.loadTrips(STIBTripsFilePath);
+        // Map <String, Route> Stib_routes = CSVReader.loadRoutes(STIBRoutesFilePath);
 
         List<Stop> Tec_stops = CSVReader.loadStops(TECStopsFilePath);
-        List<StopTime> Tec_stopTimes = CSVReader.loadStopTimes(TECStopTimesFilePath);
-        Map <String, String> Tec_trips = CSVReader.loadTrips(TECTripsFilePath);
-        Map <String, Route> Tec_routes = CSVReader.loadRoutes(TECRoutesFilePath);
+        // List<StopTime> Tec_stopTimes = CSVReader.loadStopTimes(TECStopTimesFilePath);
+        // Map <String, String> Tec_trips = CSVReader.loadTrips(TECTripsFilePath);
+        // Map <String, Route> Tec_routes = CSVReader.loadRoutes(TECRoutesFilePath);
 
         List<Node> Delijn_nodes = GraphFactory.stopsToNodes(Delijn_stops);
-        GraphFactory.buildArcs(Delijn_stopTimes, Delijn_nodes, Delijn_trips, Delijn_routes);
+        //GraphFactory.buildArcs(Delijn_stopTimes, Delijn_nodes, Delijn_trips, Delijn_routes);
 
         List<Node> Sncb_nodes = GraphFactory.stopsToNodes(Sncb_stops);
-        GraphFactory.buildArcs(Sncb_stopTimes, Sncb_nodes, Sncb_trips, Sncb_routes);
+        //GraphFactory.buildArcs(Sncb_stopTimes, Sncb_nodes, Sncb_trips, Sncb_routes);
 
         List<Node> Stib_nodes = GraphFactory.stopsToNodes(Stib_stops);
-        GraphFactory.buildArcs(Stib_stopTimes, Stib_nodes, Stib_trips, Stib_routes);
+        //GraphFactory.buildArcs(Stib_stopTimes, Stib_nodes, Stib_trips, Stib_routes);
 
         List<Node> Tec_nodes = GraphFactory.stopsToNodes(Tec_stops);
-        GraphFactory.buildArcs(Tec_stopTimes, Tec_nodes, Tec_trips, Tec_routes);
-
-        Delijn_stops = null;
-        Delijn_stopTimes = null;
-        Delijn_trips = null;
-        Delijn_routes = null;
-        Sncb_stops = null;
-        Sncb_stopTimes = null;
-        Sncb_trips = null;
-        Sncb_routes = null;
-        Stib_stops = null;
-        Stib_stopTimes = null;
-        Stib_trips = null;
-        Stib_routes = null;
-        Tec_stops = null;
-        Tec_stopTimes = null;
-        Tec_trips = null;
-        Tec_routes = null;
-        System.gc();
+        //GraphFactory.buildArcs(Tec_stopTimes, Tec_nodes, Tec_trips, Tec_routes);
 
         Graph g = new Graph();
         g.addVertices(Delijn_nodes);
